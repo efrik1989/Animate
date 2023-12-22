a2b702ccb710211950453fab85c5fc05afcd679c
Efratov Pavel <pavelefratov@gmail.com>
2023-12-22 21:37:42 +0300

    Добавил некоторые правки

--- src/Animation.java
+++ src/Animation.java
@@ -6,11 +6,16 @@ import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
+/**
+ * Класс каркас для анимаций картинки.
+ */
 public abstract class Animation extends JComponent implements Animated{
     int TIMEOUT = 100;
     BufferedImage image;
     int x = 0;
     int y = 0;
+    boolean isReverseY;
+    boolean isReverseX;
 
     public Animation(String image_path) {
         File file = new File(image_path);
--- src/QuadroAnimation.java
+++ src/QuadroAnimation.java
@@ -1,11 +1,17 @@
 import java.awt.*;
 
+/**
+ * Класс анимации картинки квадратом
+ */
 public class QuadroAnimation extends Animation{
-    int x = 0;
-    int y = 0;
+    int WIDTH = 1000;
+    int HEIGHT = 1000;
+    boolean isStopY;
+    boolean isStopX;
 
     public QuadroAnimation(String image_path) {
         super(image_path);
+        isStopY = true;
     }
 
     @Override
@@ -14,10 +20,15 @@ public class QuadroAnimation extends Animation{
         g.drawImage(image, x,y, this);
     }
 
+    //TODO: Логика не завершена. Продолжить!!!
     @Override
     public void animate() {
-        x=x+5;
-        y=y+5;
+        if (x == 1000) {
+            isStopY = false;
+            isReverseX = true;
+        }
+        if (x <= WIDTH && isStopY && !isReverseX) x=x+20;
+        y=y+20;
         repaint();
     }
 }
